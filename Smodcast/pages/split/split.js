11e22a449a468772d7d9ca647fcaba06ea46f50a
Kevin Coughlin <kevintcoughlin@gmail.com>
2013-07-02 00:54:24 -0400

    initial version, working on async rss fetching for each podcast with data promises

---
+++ Smodcast/pages/split/split.js
@@ -0,0 +1,200 @@
+﻿(function () {
+    "use strict";
+
+    var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
+    var binding = WinJS.Binding;
+    var nav = WinJS.Navigation;
+    var ui = WinJS.UI;
+    var utils = WinJS.Utilities;
+
+    ui.Pages.define("/pages/split/split.html", {
+
+        /// <field type="WinJS.Binding.List" />
+        _items: null,
+        _group: null,
+        _itemSelectionIndex: -1,
+
+        // User Navigated to Page
+        ready: function (element, options) {
+            WinJS.Utilities.startLog({ type: "info", tags: "XHR" });
+            
+            var listView = element.querySelector(".itemlist").winControl;
+
+            // Podcast (group) for Page
+            this._group = (options && options.groupKey) ? Data.resolveGroupReference(options.groupKey) : Data.groups.getAt(0);
+            
+            /// RSS Syndication Code
+            var client = new Windows.Web.Syndication.SyndicationClient();
+                client.setRequestHeader("User-Agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
+            
+            var listData = new Array();
+            
+            // Async RSS Retrieval
+            WinJS.xhr({
+                url: "http://smodcast.com/channels" + this._group.endpoint,
+                responseType: "document"
+            }).done(
+                function completed(result) {
+                    var articleSyndication = result.response;
+
+                    if (articleSyndication) {
+                        var episodes = articleSyndication.querySelectorAll("item");
+                        for (var episodeIndex = 0; episodeIndex < episodes.length; episodeIndex++) {
+                            var newEpisode = {};
+
+                            var episode = episodes[episodeIndex];
+                            
+                            newEpisode.title = episode.querySelector("title").textContent;
+                            newEpisode.subtitle = episode.querySelector("pubDate").textContent;
+                            newEpisode.description = episode.querySelector("description").textContent;
+                            newEpisode.backgroundImage = "http://smodcast.com/wp-content/blogs.dir/1/files_mf/smodcast1400.jpg";
+                            newEpisode.content = episode.querySelector("description").textContent;
+                            
+                            listData.push(newEpisode);
+                        }
+                    }
+                },
+                function error(result) {
+
+                },
+                function progress(result) {
+
+                }
+            );
+            
+            this._items = Data.getItemsFromGroup(this._group);
+            this._itemSelectionIndex = (options && "selectedIndex" in options) ? options.selectedIndex : -1;
+
+            element.querySelector("header[role=banner] .pagetitle").textContent = this._group.title;
+
+            // Prepare ListView
+            listView.itemDataSource = this._items.dataSource;
+            listView.itemTemplate = element.querySelector(".itemtemplate");
+            listView.onselectionchanged = this._selectionChanged.bind(this);
+            listView.layout = new ui.ListLayout();
+
+            this._updateVisibility();
+            
+            if (this._isSingleColumn()) {
+                if (this._itemSelectionIndex >= 0) {
+                    // For single-column detail view, load the article.
+                    binding.processAll(element.querySelector(".articlesection"), this._items.getAt(this._itemSelectionIndex));
+                }
+            } else {
+                if (nav.canGoBack && nav.history.backStack[nav.history.backStack.length - 1].location === "/pages/split/split.html") {
+                    // Clean up the backstack to handle a user snapping, navigating
+                    // away, unsnapping, and then returning to this page.
+                    nav.history.backStack.pop();
+                }
+                // If this page has a selectionIndex, make that selection
+                // appear in the ListView.
+                listView.selection.set(Math.max(this._itemSelectionIndex, 0));
+            }
+        },
+        
+        // Unload Page
+        unload: function () {
+            this._items.dispose();
+        },
+
+        // This function updates the page layout in response to viewState changes.
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            var listView = element.querySelector(".itemlist").winControl;
+            var firstVisible = listView.indexOfFirstVisible;
+            this._updateVisibility();
+
+            var handler = function (e) {
+                listView.removeEventListener("contentanimating", handler, false);
+                e.preventDefault();
+            }
+
+            if (this._isSingleColumn()) {
+                listView.selection.clear();
+                if (this._itemSelectionIndex >= 0) {
+                    // If the app has snapped into a single-column detail view,
+                    // add the single-column list view to the backstack.
+                    nav.history.current.state = {
+                        groupKey: this._group.key,
+                        selectedIndex: this._itemSelectionIndex
+                    };
+                    nav.history.backStack.push({
+                        location: "/pages/split/split.html",
+                        state: { groupKey: this._group.key }
+                    });
+                    element.querySelector(".articlesection").focus();
+                } else {
+                    listView.addEventListener("contentanimating", handler, false);
+                    if (firstVisible >= 0 && listView.itemDataSource.list.length > 0) {
+                        listView.indexOfFirstVisible = firstVisible;
+                    }
+                    listView.forceLayout();
+                }
+            } else {
+                // If the app has unsnapped into the two-column view, remove any
+                // splitPage instances that got added to the backstack.
+                if (nav.canGoBack && nav.history.backStack[nav.history.backStack.length - 1].location === "/pages/split/split.html") {
+                    nav.history.backStack.pop();
+                }
+                if (viewState !== lastViewState) {
+                    listView.addEventListener("contentanimating", handler, false);
+                    if (firstVisible >= 0 && listView.itemDataSource.list.length > 0) {
+                        listView.indexOfFirstVisible = firstVisible;
+                    }
+                    listView.forceLayout();
+                }
+
+                listView.selection.set(this._itemSelectionIndex >= 0 ? this._itemSelectionIndex : Math.max(firstVisible, 0));
+            }
+        },
+
+        // This function checks if the list and details columns should be displayed
+        // on separate pages instead of side-by-side.
+        _isSingleColumn: function () {
+            var viewState = Windows.UI.ViewManagement.ApplicationView.value;
+            return (viewState === appViewState.snapped || viewState === appViewState.fullScreenPortrait);
+        },
+
+        _selectionChanged: function (args) {
+            var listView = document.body.querySelector(".itemlist").winControl;
+            var details;
+            // By default, the selection is restriced to a single item.
+            listView.selection.getItems().done(function updateDetails(items) {
+                if (items.length > 0) {
+                    this._itemSelectionIndex = items[0].index;
+                    if (this._isSingleColumn()) {
+                        // If snapped or portrait, navigate to a new page containing the
+                        // selected item's details.
+                        nav.navigate("/pages/split/split.html", { groupKey: this._group.key, selectedIndex: this._itemSelectionIndex });
+                    } else {
+                        // If fullscreen or filled, update the details column with new data.
+                        details = document.querySelector(".articlesection");
+                        binding.processAll(details, items[0].data);
+                        details.scrollTop = 0;
+                    }
+                }
+            }.bind(this));
+        },
+
+        // This function toggles visibility of the two columns based on the current
+        // view state and item selection.
+        _updateVisibility: function () {
+            var oldPrimary = document.querySelector(".primarycolumn");
+            if (oldPrimary) {
+                utils.removeClass(oldPrimary, "primarycolumn");
+            }
+            if (this._isSingleColumn()) {
+                if (this._itemSelectionIndex >= 0) {
+                    utils.addClass(document.querySelector(".articlesection"), "primarycolumn");
+                    document.querySelector(".articlesection").focus();
+                } else {
+                    utils.addClass(document.querySelector(".itemlistsection"), "primarycolumn");
+                    document.querySelector(".itemlist").focus();
+                }
+            } else {
+                document.querySelector(".itemlist").focus();
+            }
+        }
+    });
+})();
