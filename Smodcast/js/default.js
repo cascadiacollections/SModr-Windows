7b85a6305ad8755dab9229448fde67a1684b1a40
Kevin Coughlin <kevintcoughlin@gmail.com>
2014-01-31 08:55:33 -0800

    adding background audio capability

--- Smodcast/js/default.js
+++ Smodcast/js/default.js
@@ -8,6 +8,11 @@
     var currentlyPlaying = {};
     var playingIndex = 0;
 
+    // Media Controls
+    var systemMediaControls;
+    var player;
+
+
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
@@ -27,6 +32,12 @@
                 .then(function completed() {
                     var episodesListView = document.getElementById('iconTextApplications').winControl,
                         episodeListItemTemplate = document.getElementById('iconTextApplicationsTemplate');
+
+                    player = document.getElementById("player");
+                    player.addEventListener("ended", mediaEnded, false);
+                    player.addEventListener("playing", mediaPlaying, false);
+                    player.addEventListener("pause", mediaPaused, false);
+                    player.addEventListener("error", mediaError, false);
 
                     episodesListView.itemTemplate = episodeListItemTemplate;
                     episodesListView.itemDataSource = episodesList.dataSource;
@@ -58,27 +69,16 @@
                             nowPlaying.innerHTML = "<strong>Now Playing: </strong>" + item.data.title;
                             */
 
-                            var media = document.getElementById("mediaAudio");
-                            media.src = item.data.mediaUrl;
-                            media.play();
+                            player.src = item.data.mediaUrl;
+                            player.play();
                         });
+
+                        systemMediaControls = Windows.Media.SystemMediaTransportControls.getForCurrentView();
+                        systemMediaControls.onbuttonpressed = systemMediaControlsButtonPressed;
+                        systemMediaControls.IsPlayEnabled = true;
+                        systemMediaControls.IsPauseEnabled = true;
                     };
 
-                    // Event handler for SystemMediaTransportControls' buttonpressed event
-                    function systemMediaControlsButtonPressed(eventIn) {
-                        var mediaButton = Windows.Media.SystemMediaTransportControlsButton;
-                        switch (eventIn.button) {
-                            case mediaButton.play:
-                                playMedia();
-                                break;
-                            case mediaButton.pause:
-                                pauseMedia();
-                                break;
-                            case mediaButton.stop:
-                                stopMedia();
-                                break;
-                        }
-                    }
                 })
             )}
     };
@@ -96,33 +96,38 @@
 
     // Plays the audio.
     function playMedia() {
-        var media = document.getElementById("mediaAudio");
-        media.play();
+        player.play();
     }
 
     // Pauses the audio.
     function pauseMedia() {
-        var media = document.getElementById("mediaAudio");
-        media.pause();
+        player.pause();
     }
 
     // Stops the audio.
     function stopMedia() {
-        var media = document.getElementById("mediaAudio");
-        media.pause();
-        media.currentTime = 0;
+        player.pause();
+        player.currentTime = 0;
     }
 
     // Event handlers for <audio>
     function mediaPaused(e) {
+        player.autoplay = false;
+        systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.paused;
         pauseMedia();
     }
 
     function mediaPlaying(e) {
+        player.autoplay = true;
+        systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.playing;
         playMedia();
     }
 
     function mediaEnded(e) {
+        // End of playlist reached.  We'll just stop media playback. 
+        player.autoplay = false;
+        stopMedia();
+        WinJS.log && WinJS.log("end of playlist, stopping playback", "sample", "status");
         /* 
         TODO: Implement play next functionality
         var media = document.getElementById("mediaAudio");
@@ -136,13 +141,110 @@
         episodesListView.selection.set(null);
     }
 
+    function mediaError(e) {
+        systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.closed;
+    }
+
+    // Invoked from this Page's "ready" method.  Retrieve and initialize the  
+    // SystemMediaTransportControls object. 
+    function setupSystemMediaTransportControls() {
+        // Retrieve the SystemMediaTransportControls object associated with the current app view 
+        // (ie. window).  There is exactly one instance of the object per view, instantiated by 
+        // the system the first time getForCurrentView() is called for the view.  All subsequent  
+        // calls to getForCurrentView() from the same view (eg. from different scenario pages in  
+        // this sample) will return the same instance of the object. 
+        systemMediaControls = Windows.Media.SystemMediaTransportControls.getForCurrentView();
+
+        // This scenario will always start off with no media loaded, so we will start off disabling the  
+        // system media transport controls.  Doing so will hide the system UI for media transport controls 
+        // from being displayed, and will prevent the app from receiving any events such as buttonpressed  
+        // from it, regardless of the current state of event registrations and button enable/disable states. 
+        // This makes isEnabled a handy way to turn system media transport controls off and back on, as you  
+        // may want to do when the user navigates to and away from certain parts of your app. 
+        systemMediaControls.isEnabled = false;
+
+        // To receive notifications for the user pressing media keys (eg. "Stop") on the keyboard, or  
+        // clicking/tapping on the equivalent software buttons in the system media transport controls UI,  
+        // all of the following needs to be true: 
+        //     1. Register for buttonpressed event on the SystemMediaTransportControls object. 
+        //     2. isEnabled property must be true to enable SystemMediaTransportControls itself. 
+        //        [Note: isEnabled is initialized to true when the system instantiates the 
+        //         SystemMediaTransportControls object for the current app view.] 
+        //     3. For each button you want notifications from, set the corresponding property to true to 
+        //        enable the button.  For example, set isPlayEnabled to true to enable the "Play" button  
+        //        and media key. 
+        //        [Note: the individual button-enabled properties are initialized to false when the 
+        //         system instantiates the SystemMediaTransportControls object for the current app view.] 
+        // 
+        // Here we'll perform 1, and 3 for the buttons that will always be enabled for this scenario (Play, 
+        // Pause, Stop).  For 2, we purposely set isEnabled to false to be consistent with the scenario's  
+        // initial state of no media loaded.  Later in the code where we handle the loading of media 
+        // selected by the user, we will enable SystemMediaTransportControls. 
+        systemMediaControls.addEventListener("buttonpressed", systemMediaControlsButtonPressed, false);
+
+        // Note: one of the prerequisites for an app to be allowed to play audio while in background,  
+        // is to enable handling Play and Pause buttonpressed events from SystemMediaTransportControls. 
+        systemMediaControls.isPlayEnabled = true;
+        systemMediaControls.isPauseEnabled = true;
+        systemMediaControls.isStopEnabled = true;
+        systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.closed;
+    }
+
+    // For supported audio and video formats for Windows Store apps, see: 
+    //     http://msdn.microsoft.com/en-us/library/windows/apps/hh986969.aspx 
+    var supportedAudioFormats = [
+        ".3g2", ".3gp2", ".3gp", ".3gpp", ".m4a", ".mp4", ".asf", ".wma", ".aac", ".adt", ".adts", ".mp3", ".wav", ".ac3", ".ec3",
+    ];
+
+    // this scenario's event handler for SystemMediaTransportControls' buttonpressed event 
+    function systemMediaControlsButtonPressed(eventIn) {
+        // Check if the user is still on scenario 1, or has started going into  
+        // another scenario in the sample. 
+        if (!isScenario1Active) {
+            return;
+        }
+
+        var mediaButton = Windows.Media.SystemMediaTransportControlsButton;
+        switch (eventIn.button) {
+            case mediaButton.play:
+                WinJS.log && WinJS.log("Play pressed", "sample", "status");
+                player.play();
+                break;
+
+            case mediaButton.pause:
+                WinJS.log && WinJS.log("Pause pressed", "sample", "status");
+                player.pause();
+                break;
+
+            case mediaButton.stop:
+                WinJS.log && WinJS.log("Stop pressed", "sample", "status");
+                stopPlayer();
+                break;
+
+            case mediaButton.next:
+                WinJS.log && WinJS.log("Next pressed", "sample", "status");
+                //setNewMediaItem(currentItemIndex + 1);
+                break;
+
+            case mediaButton.previous:
+                WinJS.log && WinJS.log("Previous pressed", "sample", "status");
+                //setNewMediaItem(currentItemIndex - 1);
+                break;
+
+                // Insert additional case statements for other buttons you want to handle in your app. 
+                // Remember that you also need to first enable those buttons via the corresponding 
+                // is****Enabled property on the SystemMediaTransportControls object. 
+        }
+    }
+
     // Public Members
     WinJS.Namespace.define("Smodr", {
         episodesList: episodesList,
         currentlyPlaying: currentlyPlaying,
         mediaPaused: mediaPaused,
         mediaPlaying: mediaPlaying,
-        mediaEnded: mediaEnded
+        mediaEnded: mediaEnded,
+        mediaError: mediaError
     });
 
 })();
